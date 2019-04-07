0d88234b784bcdeaa63ec0781586d05d8008671a
Sokrady Chey <sokradychey@gmail.com>
2019-04-07 07:39:07 -0400

    update package

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: "Sokrady S Chey",
+    author: "Sokrady Chey",
+    description: "A Gatsby.js Starter based on Photon by HTML5 UP"
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline'
+  ],
+}
