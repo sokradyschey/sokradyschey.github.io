5cf9d8e43fcdaad5c745d3dd9c501b085ae748ce
Sokrady Chey <sokradychey@gmail.com>
2019-04-07 00:54:48 -0400

    Switching to React Gatsby

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/sokradychey/Documents/GitHub/sokradyschey.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/sokradychey/Documents/GitHub/sokradyschey.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
