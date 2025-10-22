897482ba3cb38ef6f1442cd611a85bb8fef05f5c
Sokrady Chey <sokradychey@gmail.com>
2025-10-22 19:28:39 -0400

    Updating Job Title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <Container>
       <h1 className="display-4 fw-bold">Hi, I’m Sokrady 👋</h1>
-      <p className="fs-4">UI/UX Software Developer & QA Analyst</p>
+      <p className="fs-4">Frontend Software Developer</p>
       <p className="mb-4 fs-5">
         I create beautiful, functional web applications that solve real-world problems. 
         Let's build something amazing together.
